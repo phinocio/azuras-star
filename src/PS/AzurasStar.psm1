713a5d86b26432cc18aa5da1450725366254a921
Matt Lishman <matt.lishman@gmail.com>
2020-02-04 20:29:30 +0000

    Move all files to more closely represent the built release version
     - Make testing easier
     - Reduce complexity of code by breaking it apart

---
+++ src/PS/AzurasStar.psm1
@@ -0,0 +1,18 @@
+class AzurasStar {
+    static [string] $Name
+    static [string] $installerPath
+    static [string] $installerSrc
+    static $Icon
+    static [string] $installerDownloads
+    AzurasStar() {
+        [AzurasStar]::Name = "Azura's Star"
+        [AzurasStar]::installerPath = (Get-Item .\).FullName
+        [AzurasStar]::installerSrc = "$([AzurasStar]::installerPath)\src"
+        $iconLocation = "$([AzurasStar]::installerSrc)\img\azura.ico"
+        [AzurasStar]::Icon = New-Object system.drawing.icon($iconLocation)
+
+        $downloads = "$([AzurasStar]::installerPath)\Downloads"
+        New-Item -ItemType Directory -Path $downloads -Force
+        [AzurasStar]::installerDownloads = (Get-Item $downloads).FullName
+    }
+}
