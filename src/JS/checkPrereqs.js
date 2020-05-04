e4542473d3b5e8dcee5905be6b35dd27c3543196
Matt Lishman <matt.lishman@gmail.com>
2020-05-04 01:01:17 +0100

    Fix checking prerequisites
    - Add parser for PS objects to JS objects

--- src/JS/checkPrereqs.js
+++ src/JS/checkPrereqs.js
@@ -1,29 +1,50 @@
-function isJavaInstalled() 
-{
-    ps.addCommand('if(Get-ItemProperty HKLM:\\Software\\Microsoft\\Windows\\CurrentVersion\\Uninstall\\* | Where-Object DisplayName -like "Java*") { if(Test-Path "$env:ProgramFiles\\Java") {echo "Java is installed."} else {echo "Java is 32-Bit. 64-Bit Java is required."}} else {echo "Java is not installed!"}');
-    return ps.invoke()
-        .then(output => {
-            console.log(output);
-            document.getElementById('check-java').innerText = output;
-            ps.clear();
-        })
-        .catch(err => {
-            console.log(err);
-            ps.clear();
-        })
-}
+/**
+ * Converts a PowerShell Object string into a JS Object.
+ * PowerShell objects print with each property on a new line, this can be used to parse the output.
+ * @param PSObjectString {string} - The object in format "key    : value"
+ * @returns {{}} - Object containing key value pairs matching PS native objects
+ */
+const parsePSObjectString = PSObjectString => PSObjectString
+  .split(/\r?\n/)
+  .reduce((aggregator, PSProperty) => {
+    // Split by the first ":" to get the key/value
+    let [key, value] = PSProperty.split(/:(.+)/);
+    if (!key) {
+      // No key so just ignore
+      // Possibly empty line printed by PS output
+      return aggregator;
+    }
+    value = value || '';
+    value = value.length > 0 ? value.trim() : '';
+    return Object.assign({}, aggregator, {[key.trim()]: value})
+  }, {});
 
-function is7ZipInstalled() 
-{
-    ps.addCommand('if(Get-ItemProperty HKLM:\\Software\\Microsoft\\Windows\\CurrentVersion\\Uninstall\\* | Where-Object DisplayName -like "7-Zip*") {echo "7-Zip is installed."} else {echo "7-Zip is not installed!"}');
-    return ps.invoke()
-        .then(output => {
-            console.log(output);
-            document.getElementById('check-7Zip').innerText = output
-            ps.clear();
-        })
-        .catch(err => {
-            console.log(err);
-            ps.clear();
-        })
-}
+const getProgramInstallStatus = async program => {
+  const details = await getProgramDetailsFromPS(program);
+  console.log(details);
+  // If the object is empty, no program information can be found so return false
+  return Object.keys(details).length !== 0
+};
+
+const getProgramDetailsFromPS = async program => {
+  await ps.addCommand(`return Get-ItemProperty HKLM:\\Software\\Microsoft\\Windows\\CurrentVersion\\Uninstall\\* | Where-Object DisplayName -like "${program}*"`);
+  return parsePSObjectString(await ps.invoke())
+};
+
+
+const checkPreReqs = async () => {
+  const prerequisites = [
+    'Java',
+    '7-Zip'
+  ];
+
+  for (const program of prerequisites) {
+    const programInstallStatus = await getProgramInstallStatus(program);
+    if (programInstallStatus === false) {
+      console.log(`${program} is not installed`);
+    } else {
+      console.log(`${program} is installed correctly`);
+
+    }
+  }
+};
