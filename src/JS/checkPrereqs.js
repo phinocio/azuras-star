961fc2f4f2416e338e0391f13207a10ae94d4f4a
Matt Lishman <matt.lishman@gmail.com>
2020-05-04 21:11:12 +0100

    Add prettier as pre commit hook

--- src/JS/checkPrereqs.js
+++ src/JS/checkPrereqs.js
@@ -4,9 +4,8 @@
  * @param PSObjectString {string} - The object in format "key    : value"
  * @returns {{}} - Object containing key value pairs matching PS native objects
  */
-const parsePSObjectString = PSObjectString => PSObjectString
-  .split(/\r?\n/)
-  .reduce((aggregator, PSProperty) => {
+const parsePSObjectString = (PSObjectString) =>
+  PSObjectString.split(/\r?\n/).reduce((aggregator, PSProperty) => {
     // Split by the first ":" to get the key/value
     let [key, value] = PSProperty.split(/:(.+)/);
     if (!key) {
@@ -14,12 +13,15 @@ const parsePSObjectString = PSObjectString => PSObjectString
       // Possibly empty line printed by PS output
       return aggregator;
     }
-    value = value || '';
-    value = value.length > 0 ? value.trim() : '';
+    value = value || "";
+    value = value.length > 0 ? value.trim() : "";
     return Object.assign({}, aggregator, { [key.trim()]: value });
   }, {});
 
-const getProgramDetailsFromPS = async program => {
-  await ps.addCommand(`return Get-ItemProperty HKLM:\\Software\\Microsoft\\Windows\\CurrentVersion\\Uninstall\\* | Where-Object DisplayName -like "${program}*"`);
+const getProgramDetailsFromPS = async (program) => {
+  await ps.addCommand(
+    `return Get-ItemProperty HKLM:\\Software\\Microsoft\\Windows\\CurrentVersion\\Uninstall\\* | Where-Object DisplayName -like "${program}*"`
+  );
   return parsePSObjectString(await ps.invoke());
 };
+p;
